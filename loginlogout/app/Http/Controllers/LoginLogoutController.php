6a9ad28a9c474d23517116ef4c271ac8b8da62e2
Aaliyah Jade Ocden <166033585+AjadeOcden@users.noreply.github.com>
2025-04-06 22:49:56 +0800

    okey na toh

---
+++ loginlogout/app/Http/Controllers/LoginLogoutController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+//use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class LoginLogoutController extends Controller
+{
+    public function registrationForm(){
+        return view('registrationForm');
+    }
+    public function register(Request $request){
+        // Validate input
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        if($validator ->fails()){
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+       $name = $request->input('name');
+       $email = $request->input('email');
+       $password = Hash::make($request->input('password'));
+
+       DB::insert('INSERT INTO users (name, email, password, created_at, updated_at) Values(?,?,?,?,?)', [$name,$email,$password, now(), now()]);
+
+       return redirect()->route('login')->with('success', 'Registration Successful!');
+    }
+
+    public function loginForm(){
+        return view("loginForm");
+    }
+    
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+    
+        $email = $request->input("email");
+        $password = $request->input("password");
+    
+        $user = DB::select("SELECT * FROM users WHERE email = ?", [$email]);
+    
+        if ($user) {
+            if (Hash::check($password, $user[0]->password)) {
+                
+                    Auth::loginUsingId($user[0]->id);
+                    return redirect("home");
+                
+            } else {
+                return back()->with("error", "Invalid password.");
+            }
+        } else {
+            return back()->with("error", "User not found.");
+        }
+    }
+
+    public function home(){
+        return view("home");
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+
+
+}
---
+++ loginlogout/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+use App\Http\Controllers\LoginLogoutController;
+
+Route::get("/register", [LoginLogoutController::class, "registrationForm"])->name('register');
+Route::post("/register", [LoginLogoutController::class, "register"]);
+
+Route::get("/login", [LoginLogoutController::class, "loginForm"])->name('login');
+Route::post("/login", [LoginLogoutController::class, "login"]);
+Route::post("/logout", [LoginLogoutController::class, "logout"])->name('logout');
+
+Route::get("/home", [LoginLogoutController::class, "home"]);
+Route::middleware('auth')->get('/home', function(){
+return view('home');
+});
+
